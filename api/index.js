a9f593f6f7a2c42abdcd660e95d6d9b60ed4d3d5
akash kumar <iamaakashkumar789@gmail.com>
2023-10-15 14:50:10 +0530

    new commit

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+// add type:module in package-json to use import instead or require
+const app= express();
+app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+
+}
+
+);
